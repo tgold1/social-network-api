72057ae7c9fe6a923d03f1b969179240b85417f8
Tim Gold <timothygold@comcast.net>
2023-05-13 16:02:36 -0400

    added to thoughtController.js to each of the get, post, delete, and update thought routes

---
+++ controllers/thoughtController.js
@@ -0,0 +1,58 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+    getThoughts(req, res) {
+        Thought.find()
+        .populate('users')
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    getSingleThought(req, res) {
+        Thought.findOne({_id: req.params.thoughtId})
+            .populate('users')
+            .select('-__v')
+            .then((thought) => 
+              !thought
+                ? res.status(404).json({ message: 'No thought with that ID' })
+                : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    createThought(req, res) {
+        Thought.create(req.body)
+          .then((thought) => res.json(thought))
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+    },
+
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({_id: req.params.thoughtId })  
+          .then((thought) => 
+            !thought 
+              ? res.status(404).json({ message: 'No thought with that ID'})
+              : res.json({ message: 'Thought successfully deleted'})
+          )
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true}
+        )
+          .then((thought) => 
+            !thought
+              ? res.status(404).json({ message: 'No thought with this ID!'})
+              : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    }
+}
